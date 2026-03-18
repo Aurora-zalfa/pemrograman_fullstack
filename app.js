9ebd0130b15582928d00fa1dd1453403c6eb46fa
Aurora Zalfa Hartono <aurorazalhartono@gmail.com>
2026-03-18 12:40:40 +0700

    setup upload route in app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
+// import routes dari Rum
+const uploadRoutes = require('./routes/uploadRoutes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// biar file upload bisa diakses
+app.use('/uploads', express.static('uploads'));
+
+// daftarin endpoint upload
+app.use('/api/upload', uploadRoutes);
 
 app.get("/", (req,res)=>{
   res.send("API Monitoring Sawit Running");
