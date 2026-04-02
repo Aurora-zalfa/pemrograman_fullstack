935ab5423f0d6e06b61960bf769e323f47751be8
zainabaznur <zainabna05@gmail.com>
2026-04-02 13:03:13 +0700

    feat: menambah endpoint api data master supir, truk, kebun, pabrik, dan users

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/database"); // Tambahkan ini
+const masterRoutes = require("./routes/master"); // Tambahkan ini
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Daftarkan route di sini
+app.use("/api", masterRoutes); 
 
 app.get("/", (req,res)=>{
   res.send("API Monitoring Sawit Running");
