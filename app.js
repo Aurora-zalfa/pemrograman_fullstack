4662da17f246003948e5592c1827ebff2eb6ca7b
yanti <yantielnayaputri@gmail.com>
2026-04-02 12:12:09 +0700

    Menambahkan fitur laporan harian

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const laporanRoutes = require('./routes/laporan');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/laporan-harian', laporanRoutes);
 
 app.get("/", (req,res)=>{
   res.send("API Monitoring Sawit Running");
@@ -13,3 +15,4 @@ app.get("/", (req,res)=>{
 app.listen(3000, ()=>{
   console.log("Server running on port 3000");
 });
+
